1f185be505df27d57bab24ee9dfc6cae1090b633
MhmdSyd <mhmdsyd.workout@gmail.com>
2025-04-28 22:35:37 +0300

    update welcome endpoint response

--- src/routes/base.py
+++ src/routes/base.py
@@ -12,7 +12,7 @@ async def welcome(app_settings: Settings = Depends(get_settings)):
 
     
     APP_NAME = app_settings.APP_NAME
-    APP_VERSION = app_settings.APP_NAME
+    APP_VERSION = app_settings.APP_VERSION
     
     return {
         'APP_NAME': APP_NAME,
